4422006712a19b2f110bff66d5b019e01ef57fc7
SoinRoma <soinroma26@gmail.com>
2024-05-23 13:10:32 +0500

    - added loader for result

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,6 +1,9 @@
-function Result({checkboxes, setAnalyses}) {
+import {useState} from "react";
 
+function Result({checkboxes, setAnalyses}) {
+  const [isLoader, setIsLoader]  = useState(false)
   async function getAnalyses() {
+    setIsLoader(true)
     const response = await fetch('https://it-gen.altek.uz/api/v1/directory/analyses/list/', {
       method: 'POST',
       body: JSON.stringify({symptom_list: checkboxes}),
@@ -10,6 +13,7 @@ function Result({checkboxes, setAnalyses}) {
     })
     if (response.ok) {
       const data = await response.json()
+      setIsLoader(false)
       setAnalyses(data.analysis)
     }
   }
@@ -17,7 +21,12 @@ function Result({checkboxes, setAnalyses}) {
   return (
     <div className="result-block">
       {checkboxes.length > 0 &&
-        <button onClick={() => getAnalyses()}>Получить назначение</button>
+        <button onClick={() => getAnalyses()}>
+          <span>Получить назначение</span>
+          {isLoader &&
+            <img src="./spinner.gif" width={25} alt="spinner"/>
+          }
+        </button>
       }
     </div>
   )
