bce4ecc74765df7e6be6ba30757556e54f5f3b6a
SoinRoma <soinroma26@gmail.com>
2024-05-23 13:07:36 +0500

    - added loader for category

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -1,12 +1,20 @@
-function CategoryList({categories, setSteps, setAnalyses, setCheckboxes}) {
+import {useState} from "react"
 
+function CategoryList({categories, setSteps, setAnalyses, setCheckboxes}) {
+  const [isLoader, setIsLoader]  = useState(false)
+  const [activeId, setActiveId] = useState(0)
   async function openCategory(id) {
-    const response = await fetch(`https://it-gen.altek.uz/api/v1/directory/category/${id}/`)
-    if (response.ok) {
-      const data = await response.json()
-      setSteps(data.results)
-      setAnalyses([])
-      setCheckboxes([])
+    if (activeId !== id){
+      setIsLoader(true)
+      setActiveId(id)
+      const response = await fetch(`https://it-gen.altek.uz/api/v1/directory/category/${id}/`)
+      if (response.ok) {
+        const data = await response.json()
+        setIsLoader(false)
+        setSteps(data.results)
+        setAnalyses([])
+        setCheckboxes([])
+      }
     }
   }
 
@@ -15,7 +23,12 @@ function CategoryList({categories, setSteps, setAnalyses, setCheckboxes}) {
       <h3 className="category-title">Категория заболеваний</h3>
       <ul>
         {categories.map(category =>
-          <li key={category.id} onClick={() => openCategory(category.id)}>{category.name}</li>
+          <li key={category.id} onClick={() => openCategory(category.id)}>
+            <span>{category.name}</span>
+            {isLoader && activeId === category.id &&
+              <img src="./spinner.gif" width={25} alt="spinner"/>
+            }
+          </li>
         )}
       </ul>
     </div>
